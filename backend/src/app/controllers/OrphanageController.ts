48d3acc17f1963673d92ee2a9155676b8f8e0763
Dyego Soriano <dyegodf2@gmail.com>
2020-11-06 15:09:24 -0300

    validando campos de cadastro de orfanatos

--- backend/src/app/controllers/OrphanageController.ts
+++ backend/src/app/controllers/OrphanageController.ts
@@ -1,9 +1,13 @@
 import { Request, Response } from 'express'
 import { getRepository } from 'typeorm'
+import * as Yup from 'yup'
 
 import orphanageView from '../views/orphanages_view'
 import Orphanage from '@models/Orphanage'
 
+interface ValidationErrors {
+  [key: string]: string[]
+}
 class OrphanageController {
   async store(req: Request, res: Response) {
     const {
@@ -20,9 +24,7 @@ class OrphanageController {
     const images = requestImages.map(img => ({ path: img.filename }))
 
     try {
-      const repoOrphanage = getRepository(Orphanage)
-
-      const orphanage = repoOrphanage.create({
+      const data = {
         open_on_weekends,
         opening_hours,
         instructions,
@@ -31,12 +33,42 @@ class OrphanageController {
         images,
         about,
         name,
+      }
+
+      const schema = Yup.object().shape({
+        open_on_weekends: Yup.boolean().required(),
+        opening_hours: Yup.string().required(),
+        instructions: Yup.string().required(),
+        longitude: Yup.number().required(),
+        latitude: Yup.number().required(),
+        name: Yup.string().required(),
+        about: Yup.string().required().max(300),
+        images: Yup.array(
+          Yup.object().shape({
+            path: Yup.string().required(),
+          }),
+        ),
       })
 
+      await schema.validate(data, { abortEarly: false })
+
+      const repoOrphanage = getRepository(Orphanage)
+      const orphanage = repoOrphanage.create(data)
       const orphanageCreated = await repoOrphanage.save(orphanage)
 
       return res.status(200).json(orphanageCreated)
     } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        const errors: ValidationErrors = {}
+
+        error.inner.forEach(err => {
+          errors[err.path] = err.errors
+          console.log(err.errors)
+        })
+
+        return res.status(400).json({ message: `Validation fails`, errors })
+      }
+
       console.log(`error.message >>> ${error.message} <<<`)
 
       return res
