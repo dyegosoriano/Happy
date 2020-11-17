af199abda0003209f7df6e64cb0efa475cfb50c3
Dyego Soriano <dyegodf2@gmail.com>
2020-11-17 13:09:11 -0300

    refatorando errorHandling

--- backend/src/app/middleware/errorHandling.ts
+++ backend/src/app/middleware/errorHandling.ts
@@ -1,17 +1,17 @@
 import { NextFunction, Request, Response } from 'express'
 
 export default {
-  notFound(req: Request, resp: Response, next: NextFunction): void {
+  notFound(req: Request, res: Response, next: NextFunction): void {
     const error: any = new Error('Not found')
     error.status = 404
 
     next(error)
   },
 
-  cathAll(error: any, req: Request, resp: Response, next: NextFunction): void {
+  cathAll(error: any, req: Request, res: Response, next: NextFunction): void {
     console.log(`error.message >>> ${error.message} <<<`)
 
-    resp
+    res
       .status(error.status || 500)
       .json({ error: `Sorry! There was an error on our server.` })
   },
