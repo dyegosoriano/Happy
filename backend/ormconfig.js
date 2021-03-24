67f00f9206b587df6b9b0fe7c4fb20a74af9b7d1
Dyego Soriano <dyegodf2@gmail.com>
2021-03-24 19:40:32 -0300

    adicionando variáveis de ambiente

--- backend/ormconfig.js
+++ backend/ormconfig.js
@@ -1,14 +1,16 @@
+const rootDir = process.env.NODE_ENV === 'development' ? 'src' : 'dist'
+
 module.exports = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'docker',
-  database: 'happy',
-  synchronize: false,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: process.env.DB_SINCRONIZE === 'true' ? true : false,
   logging: false,
-  migrations: ['src/database/migrations/*.{js,ts}'],
-  entities: ['src/app/models/*.{js,ts}'],
+  migrations: [`${rootDir}/database/migrations/*.{js,ts}`],
+  entities: [`${rootDir}/app/models/*.{js,ts}`],
   cli: {
     migrationsDir: [`src/database/migrations/`],
     entitiesDir: [`src/app/models/`],
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,27 @@
-import app from './app'
+import { resolve } from 'path'
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
 
-const port = 3333
+import './database'
+
+import errorHandling from './app/middleware/errorHandling'
+import logRequest from './app/middleware/logRequest'
+import routes from './routes'
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 3333
+
+app.use(cors())
+app.use(express.json())
+app.use(logRequest)
+
+app.use('/uploads', express.static(resolve(__dirname, '..', 'uploads')))
+app.use(routes)
+
+app.use(errorHandling.notFound)
+app.use(errorHandling.cathAll)
 
 app.listen(port, () => console.log(`🚀  Server is running port: ${port}`))
